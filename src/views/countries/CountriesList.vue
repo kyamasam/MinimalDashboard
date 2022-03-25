af1a8257321ed8bda136a302eba6c9e499a23b27
SamK <samuelkyama102@gmail.com>
2022-03-25 23:55:09 +0300

    use new datatable component

--- src/views/countries/CountriesList.vue
+++ src/views/countries/CountriesList.vue
@@ -1,19 +1,33 @@
 <template>
 <div>
-  <p>countries</p>
-  <p>{{countries}}</p>
+  <dashboard-layout :title="title">
+    <data-table :table_titles="['country code', 'name']" >
+      <tr class="text-gray-700 dark:text-gray-400">
+        <td v-for="(index, item) in table_data" :key="index" class="px-4 py-3">{{item}}</td>
+      </tr>
+    </data-table>
+
+  </dashboard-layout>
+
 </div>
 </template>
 
 <script>
 import {mapGetters} from "vuex";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+import DataTable from "@/components/data/DataTable";
 
 export default {
   name: "CountriesList",
+  components: {DataTable, DashboardLayout},
   data(){
     return {
-      countries:[]
+      countries:[],
+      table_data: []
     }
+  },
+  props:{
+    title:{type:String, default:''}
   },
   computed:{
     ...mapGetters(['getCountries'])
