f1e8f8500bcfb39ec785ebc7645ed41cb7325095
SamK <samuelkyama102@gmail.com>
2022-03-25 23:48:31 +0300

    add routes and child routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,42 @@
 import CountriesList from "@/views/countries/CountriesList";
 import { createWebHistory, createRouter } from "vue-router";
 import PageNotFound from './views/utils/PageNotFound'
+import EditCountry from "@/views/countries/EditCountry";
+import ViewProfile from "@/views/profile/ViewProfile";
+import EditProfile from "@/views/profile/EditProfile";
+import Login from "@/views/Login";
 const routes = [
-    { path: '/countries', name:'Countries', component: CountriesList },
+    {
+        path: '/countries',
+        name:'Countries',
+        component: CountriesList,
+        props:{
+          title:'Countries'
+        },
+        children:[
+            {
+                path:'edit/:id',
+                name:'editCountry',
+                component: EditCountry
+            },
+        ]
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: ViewProfile,
+        props:{
+          title:'Profile'
+        },
+        children: [
+            {
+                path: 'edit/:id',
+                name: 'editProfile',
+                component: EditProfile
+            }
+        ]
+    },
+    {path: '/', name:'Login', component: Login},
     { path: '/:pathMatch(.*)*', name:'PageNotFound', component: PageNotFound },
 ]
 
