5176fa8377f38906a58a9e4464a6c96bcb4de57c
SamK <samuelkyama102@gmail.com>
2022-03-25 23:53:56 +0300

    Add interceptor for passing bearer tokens

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,31 @@
 
 <script>
 
+import api from "@/api";
+import router from "@/router";
+import axios from "axios";
+
 export default {
   name: 'App',
   components: {
-  }
+  },
+  created: function () {
+    //check if user is logged in
+    api.interceptors.request.use((config) => {
+      config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+      return config;
+    });
+    api.interceptors.response.use( undefined, function (err) {
+      return new Promise(function () {
+        if (err.response.status === 401) {
+          localStorage.removeItem('token')
+          delete axios.defaults.headers.common['Authorization']
+          router.push('/')
+        }
+        throw err;
+      });
+    });
+  },
 }
 </script>
 
