b4e5aabb87a03e333076219a589e1fb475a71c21
SamK <samuelkyama102@gmail.com>
2022-03-25 23:54:47 +0300

    create button component

---
+++ src/components/actions/Button.vue
@@ -0,0 +1,18 @@
+<template>
+<button class="flex flex-row p-2 rounded-md bg-malachite-500 text-malachite-800">
+  {{name}}
+</button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props:{
+    name:{default:'', type:String}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
