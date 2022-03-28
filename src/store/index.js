75eb932f4288126dd10266549b460b5e8d9d74e7
SamK <samuelkyama102@gmail.com>
2022-03-28 11:24:32 +0300

    add login and profile to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import {createStore} from "vuex";
 import {countries} from '@/store/modules/countries'
+import {login} from '@/store/modules/login'
+import {profile} from '@/store/modules/profile'
 
 const store = createStore({
     modules: {
-        countries
+        countries,
+        login, profile
     }
 });
 
