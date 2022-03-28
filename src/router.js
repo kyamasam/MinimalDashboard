d68bc5522b64544a6918cecd740c03c0dcd835bf
SamK <samuelkyama102@gmail.com>
2022-03-28 11:17:52 +0300

    add edit and view profile routes

--- src/router.js
+++ src/router.js
@@ -1,38 +1,44 @@
 import CountriesList from "@/views/countries/CountriesList";
 import { createWebHistory, createRouter } from "vue-router";
 import PageNotFound from './views/utils/PageNotFound'
-import EditCountry from "@/views/countries/EditCountry";
 import ViewProfile from "@/views/profile/ViewProfile";
-import EditProfile from "@/views/profile/EditProfile";
 import Login from "@/views/Login";
+import CountryBase from "@/views/countries/CountryBase";
+import ViewCountry from "@/views/countries/ViewCountry";
+import BaseProfile from "@/views/profile/BaseProfile";
 const routes = [
     {
         path: '/countries',
         name:'Countries',
-        component: CountriesList,
+        component: CountryBase,
         props:{
           title:'Countries'
         },
         children:[
             {
-                path:'edit/:id',
-                name:'editCountry',
-                component: EditCountry
+               path:'',
+                name:'CountryList',
+                component: CountriesList
+            },
+            {
+                path:'view/:id',
+                name:'viewCountry',
+                component: ViewCountry
             },
         ]
     },
     {
         path: '/profile',
         name: 'Profile',
-        component: ViewProfile,
+        component: BaseProfile,
         props:{
           title:'Profile'
         },
         children: [
             {
-                path: 'edit/:id',
-                name: 'editProfile',
-                component: EditProfile
+                path: ':id',
+                name: 'viewProfile',
+                component: ViewProfile
             }
         ]
     },
