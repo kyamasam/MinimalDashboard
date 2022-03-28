26e3e4cd5a30ed3ad8d03a1c274dbf19fff03f4d
SamK <samuelkyama102@gmail.com>
2022-03-28 11:27:11 +0300

    add countries store functions

--- src/store/modules/countries.js
+++ src/store/modules/countries.js
@@ -1,7 +1,8 @@
+import api from "@/api";
+
 export const countries = {
     state: {
         countries: [
-            {'name': 'ke'}
         ]
     },
     getters: {
@@ -9,7 +10,29 @@ export const countries = {
             return state.countries
         }
     },
-    mutations: {},
-    actions: {}
+    actions: {
+        fetchCountries({commit}, payload){
+            return new Promise((resolve, reject)=>{
+                console.log("pay", payload ===undefined ? 'payload' : '' )
+                let relative_url = '/auth/countries'
+                if (payload !==undefined){
+                    relative_url+=`/${payload}`
+                }
+                console.log("rela", relative_url)
+                api.get(relative_url).then(resp=>{
+                    this.countries = resp.data
+                    resolve(resp.data)
+                    commit('setCountries', countries)
+                }).catch(err=>{
+                    commit('setError', {data: err.response.data , status: err.response.status})
+                    reject(err)
+                })
+            })
+        }
+    },
+
+    mutations: {
+        setCountries: (state, countries)=>(state.countries = countries)
+    },
 
 }
