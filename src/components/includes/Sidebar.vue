2a2409efb4186e65b82fe7c47f89789c061b2602
SamK <samuelkyama102@gmail.com>
2022-03-28 11:17:23 +0300

    modify leftbar width

--- src/components/includes/Sidebar.vue
+++ src/components/includes/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="font-gray-50 py-4">
-  <div class="px-4 py-2 bg-gray-100 text-gray-800">
-    <p class="font-semibold">Countries</p>
+<div class="font-gray-50 py-4 w-4/12">
+  <div class="px-4 py-2 bg-gray-100 text-gray-800 w-max">
+    <router-link :to="{name:'CountryList'}" class="font-semibold">Countries</router-link>
   </div>
 </div>
 </template>
