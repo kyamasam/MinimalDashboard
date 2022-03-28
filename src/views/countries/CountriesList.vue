3a93a8908768c0960d812b14c51d1b1fb1c74ea3
SamK <samuelkyama102@gmail.com>
2022-03-28 11:26:46 +0300

    fetch countries from api

--- src/views/countries/CountriesList.vue
+++ src/views/countries/CountriesList.vue
@@ -1,41 +1,71 @@
 <template>
-<div>
-  <dashboard-layout :title="title">
-    <data-table :table_titles="['country code', 'name']" >
-      <tr class="text-gray-700 dark:text-gray-400">
-        <td v-for="(index, item) in table_data" :key="index" class="px-4 py-3">{{item}}</td>
-      </tr>
-    </data-table>
+    <dashboard-layout :title="title">
+      <p v-if="success_message !== ''" class="text-malachite-500 font-bold">{{success_message}}</p>
 
-  </dashboard-layout>
+      <data-table :table_titles="['name','country code', 'phone code', 'created at', 'actions']">
+        <tr v-for="(item, index) in table_data" :key="index" class="text-gray-700 dark:text-gray-400">
+          <td class="px-4 py-3">
+            {{ item.name }}
+          </td>
+          <td class="px-4 py-3">
+            {{ item.iso3CountryCode }}
+          </td>
+          <td class="px-4 py-3">
+            {{ item.phoneCode }}
+          </td>
+          <td class="px-4 py-3">
+            {{ item.createdAt }}
+          </td>
+          <td class="flex flex-row space-x-2">
+            <router-link class="flex flex-row p-2 rounded-md bg-malachite-500 text-malachite-800 w-min " :to="{name:'viewCountry', params:{id:item.id}}">
+              View
+            </router-link>
+            <button @click="editCountry" class="flex flex-row p-2 rounded-md bg-malachite-500 text-malachite-800">
+              Edit Country
+            </button>
+          </td>
 
-</div>
+        </tr>
+      </data-table>
+      <div v-if="loading">Loading ...</div>
+    </dashboard-layout>
 </template>
 
 <script>
 import {mapGetters} from "vuex";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import DataTable from "@/components/data/DataTable";
-
+import {hasPermission} from "@/utils/methods"
 export default {
   name: "CountriesList",
   components: {DataTable, DashboardLayout},
-  data(){
+  data() {
     return {
-      countries:[],
-      table_data: []
+      countries: [],
+      success_message:'',
+      table_data: [],
+      loading:false
     }
   },
-  props:{
-    title:{type:String, default:''}
+  props: {
+    title: {type: String, default: ''}
   },
-  computed:{
+  computed: {
     ...mapGetters(['getCountries'])
   },
-  methods:{
-    retrieveCountries(){
-      this.countries = this.getCountries
-    }
+  methods: {
+    retrieveCountries() {
+      this.loading = true
+      this.$store.dispatch('fetchCountries').then(resp => {
+        this.table_data = resp.data
+      }).then(()=>{
+        this.loading=false
+      });
+    },
+    editCountry(){
+      if(hasPermission('edit_country'))
+        this.success_message = 'You have permission'
+      }
   },
   mounted() {
     this.retrieveCountries()
